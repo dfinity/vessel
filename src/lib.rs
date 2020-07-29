625430e6630b6a4e8dc76a7a5c37660fe4516786
Christoph <christoph.hegemann1337@gmail.com>
2020-07-29 10:08:47 +0200

    TODO about fetching upstream

--- src/lib.rs
+++ src/lib.rs
@@ -256,6 +256,7 @@ pub fn init() -> Result<()> {
     manifest.write_all(br#"{ dependencies = [ "base", "matchers" ] }
 "#)?;
     let mut manifest = fs::File::create("package-set.dhall")?;
+    // TODO: Fetch this upstream URL to point to the available version
     manifest.write_all(br#"let upstream =
     https://raw.githubusercontent.com/kritzcreek/vessel-package-set/b8a50b772af45877ed1d7fae929c415820790b01/src/packages.dhall sha256:2ba38db2e5454a25e91841ba93e8f7bdb4b73fd59e0e6e2f3d8550d61fa63b1c
 
