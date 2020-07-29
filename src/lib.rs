786b8a86d16c2eae55f0e474fd067742d37dfb6f
Christoph <christoph.hegemann1337@gmail.com>
2020-07-29 09:58:55 +0200

    Initializes dhall files

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use log::{debug, info, warn};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::io::Write;
 use std::iter::Iterator;
 use std::path::{Path, PathBuf};
 use std::process::Command;
@@ -251,6 +252,38 @@ pub fn init() -> Result<()> {
         ));
     }
     // "TODO: Implement creation of the dhall files
+    let mut manifest = fs::File::create("vessel.dhall")?;
+    manifest.write_all(br#"{ dependencies = [ "base", "matchers" ] }
+"#)?;
+    let mut manifest = fs::File::create("package-set.dhall")?;
+    manifest.write_all(br#"let upstream =
+    https://raw.githubusercontent.com/kritzcreek/vessel-package-set/b8a50b772af45877ed1d7fae929c415820790b01/src/packages.dhall sha256:2ba38db2e5454a25e91841ba93e8f7bdb4b73fd59e0e6e2f3d8550d61fa63b1c
+
+let Package =
+    { name : Text, version : Text, repo : Text, dependencies : List Text }
+
+let
+  -- This is where you can add your own packages to the package-set
+  additions =
+    [] : List Package
+
+let
+  {- This is where you can override existing packages in the package-set
+
+     For example, if you wanted to use version `v2.0.0` of the foo library:
+     let overrides = [
+         { name = "foo"
+         , version = "v2.0.0"
+         , repo = "https://github.com/bar/foo"
+         , dependencies = [] : List Text
+         }
+     ]
+  -}
+  overrides =
+    [] : List Package
+
+in  upstream # additions # overrides
+"#)?;
     Ok(())
 }
 
